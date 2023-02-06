1c525f1969d06908e2ae5daedaff6c625bf15059
Shane Gunawardana <shane.gunawardana@mercuryretail.com>
2023-02-06 20:22:28 +0800

    Configure vite for react development, add scaffold for TaskList

---
+++ src/resources/js/Pages/TaskList.jsx
@@ -0,0 +1,7 @@
+export default function TaskList() {
+    return (
+        <div>
+            <div>Works!</div>
+        </div>
+    );
+}
---
+++ src/resources/js/app.jsx
@@ -0,0 +1,8 @@
+import "./bootstrap";
+import React from "react";
+import ReactDOM from "react-dom/client"
+import TaskList from "./Pages/TaskList";
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <TaskList />
+)
